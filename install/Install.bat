ddc6bbc4bd6f2d16c978ef98dc1eca195617a3e6
xossrifat <151685749+xossrifat@users.noreply.github.com>
2024-12-08 08:25:54 -0800

    Add files via upload

---
+++ install/Install.bat
@@ -0,0 +1,7 @@
+cd
+cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
+installUtil.exe -i C:\Users\Admin\source\repos\svchost\svchost2\bin\Debug\svchost.exe
+cd/
+net start "svchost"
+cd
+
---
+++ install/Uninstall.bat
@@ -0,0 +1,5 @@
+cd
+cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
+installUtil.exe -u C:\Users\Admin\source\repos\svchost\svchost2\bin\Debug\svchost.exe
+cd
+
